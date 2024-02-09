a3f4cf3ad2c72268ec73f8e35b75ac89fbbc2d1e
D-A-Fernandez <fd2006711@gmail.com>
2024-02-09 09:21:28 -0500

    basic ui in

---
+++ SlingshotJumping/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    public AudioSource audioSource;
+    private string sceneName;
+    public Animator transition;
+    public float transitionTime = 1f;
+
+
+    /*IEnumerator PlayAudioAndLoadScene()
+    {
+        // Play AudioSource
+        audioSource.Play();
+        // Set trigger
+        transition.SetTrigger("Start");
+        // Wait for half a second
+        yield return new WaitForSeconds(transitionTime);
+
+        // Load your scene
+        LoadScene(sceneName)
+    }*/
+
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+    public void OnClickStart()
+    {
+        sceneName = "Basic";
+        LoadScene(sceneName);
+        //StartCoroutine(PlayAudioAndLoadScene());
+    }
+
+    public void OnClickHelp()
+    {
+        sceneName = "Help";
+        LoadScene(sceneName);
+        //StartCoroutine(PlayAudioAndLoadScene());
+    }
+
+    public void OnClickCredits()
+    {
+        sceneName = "Credits";
+        LoadScene(sceneName);
+        //StartCoroutine(PlayAudioAndLoadScene());
+    }
+
+    public void OnClickMainMenu()
+    {
+        sceneName = "TitleScreen";
+        LoadScene(sceneName);
+        //StartCoroutine(PlayAudioAndLoadScene());
+    }
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
+    }
+}
+
